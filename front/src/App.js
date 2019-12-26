1060770d41baf431ba10f0e72c831baa4b6bb387
Petra <petra.silavuori@gmail.com>
2019-12-26 18:29:26 +0200

    Set up data fetching for front.

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("http://localhost:8000/api/getPatients");
+        return setPatients(response.data)
+      } catch (err) {
+        return console.error(err);
+      }
+    }
+    fetchData();
+  }, []);
+
+  console.log("patients: ", patients)
   return (
     <div className="App">
     </div>

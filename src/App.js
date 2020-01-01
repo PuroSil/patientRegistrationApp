8ac9040b80cd6cebcf7b62bd6fe95852caded54e
Petra <petra.silavuori@gmail.com>
2020-01-01 14:14:55 +0200

    Minor changes to package and app.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Switch>
-          <Login path="/login" exact component={Login} />
+          <Login path="/" exact component={Login} />
           <Home path="/home" component={Home} />
           <Patients path="/patients" component={Patients}/>
         </Switch>
